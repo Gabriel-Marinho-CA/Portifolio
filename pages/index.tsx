d3a3726fea8fd93cf907d5b230270f8e8573e03c
Gabriel André <gabriel.andre@resultate.com.br>
2024-12-03 20:15:40 -0300

    fix: re-render error

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,10 @@ export default function Home() {
   return (
     <>
       <Head>
-      <html lang="pt-br" />
         <title>Gabriel</title>
         <meta name="description" content="Potifolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon-gm.png" /> 
+        <link rel="icon" href="/favicon-gm.png" />
 
         <link
           rel="stylesheet"
